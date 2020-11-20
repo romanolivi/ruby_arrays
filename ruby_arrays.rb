10ed7331f28ac430f941fedf13ae87e10c31bf4e
Roberto-Livi <robertomlivi@outlook.com>
2020-11-20 17:58:28 -0500

    Using an array and manipulating them

---
+++ ruby_arrays.rb
@@ -0,0 +1,12 @@
+
+games = ["COD", "God of War", "2K", "Madden"]
+
+puts games[0]
+
+puts "I want to get a game for my brother as a christmas present. Should I buy #{games[1]} or #{games[3]}?"
+
+puts games.include?("COD")
+
+puts games.reverse()
+
+
